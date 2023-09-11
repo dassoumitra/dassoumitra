92051b9760d8b4f0da6f518fb934d6bb863b941c
dassoumitra <125145966+dassoumitra@users.noreply.github.com>
2023-09-11 21:14:11 +0530

    Add files via upload

---
+++ add2.cpp
@@ -0,0 +1,12 @@
+// write a C program to add two number.
+#include<stdio.h>
+main()
+{
+	float a,b,s;
+	printf("Enter the number:" );
+	scanf("%f",&a);
+	printf("Enter the second number: ");
+	scanf("%f",&b);
+	s=a+b;
+	printf("The sum of %f & %f is %f:",a,b,s);
+}
